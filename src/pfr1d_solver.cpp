3c01cfc30749c5427e1389db876e5d2c0825b6c7
Bharat Medasani <mbkumar@gmail.com>
2019-04-27 07:28:11 -0400

    pfr1d_solver shifted to cpp from header only

---
+++ src/pfr1d_solver.cpp
@@ -0,0 +1,128 @@
+/****************************************************************************
+ * Solves the DAE numerical equations w.r.t z using IDAS
+***************************************************************************
+*/
+
+#include "pfr1d_solver.h"
+#include "pfr1d.h"
+#include "cantera/numerics/IDA_Solver.h"
+
+using namespace Cantera;
+using namespace std;
+
+namespace HeteroCt {
+
+PFR1dSolver::PFR1dSolver(PFR1d* pfr)
+{
+    m_neq = pfr->nEquations();
+    m_vec.resize(m_neq);
+    m_var = pfr->variablesNames();
+
+    try
+    {
+        m_solver = new IDA_Solver {*pfr};
+        m_solver->setJacobianType(0);
+        m_solver->setDenseLinearSolver();
+        m_solver->init(0.0);
+    }
+    catch (Cantera::CanteraError& err)
+    {
+        std::cerr << err.what() << std::endl;
+    }
+}
+
+PFR1dSolver::~PFR1dSolver()
+{
+    if (m_solver != nullptr) delete m_solver;
+}
+
+void PFR1dSolver::setTolerances(double rtol, double atol)
+{
+    m_solver->setTolerances(rtol, atol);
+}
+
+void PFR1dSolver::setMaxNumSteps(unsigned maxsteps)
+{
+    m_solver->setMaxNumSteps(maxsteps);
+}
+
+void PFR1dSolver::setInitialStepSize(double h0)
+{
+    m_solver->setInitialStepSize(h0);
+}
+
+void PFR1dSolver::setStopPosition(double tstop)
+{
+    m_solver->setStopTime(tstop);
+}
+
+int PFR1dSolver::solve(double xout)
+{ 
+    int retcode = 0;
+
+    if (!m_ss_started)
+    {
+        for (auto var : m_var) { m_ss << var << ","; }
+        m_ss << "x" << std::endl;
+
+        for (unsigned i = 0; i != m_neq; ++i) {
+            m_ss << m_solver->solution(i) << ",";
+        }
+        m_ss << 0.0 << std::endl;
+
+        m_ss_started = true;
+    }
+
+    // TODO Manage return codes from IDA_Solver.solve.
+    try
+    {
+        retcode = m_solver->solve(xout);
+    }
+    catch (CanteraError& err)
+    {
+        std::cerr << err.what() << std::endl;
+        retcode = -99;
+    }
+
+    // TODO get pointer to sol instead.
+    m_ss << xout << ",";
+    cout << xout << endl;
+    for (unsigned i = 0; i != m_neq; ++i) {
+        m_ss << m_solver->solution(i) << ",";
+    }
+    m_ss << std::endl;
+
+    return retcode;
+}
+
+double PFR1dSolver::solution(unsigned num) const
+{
+    return m_solver->solution(num);
+}
+
+vector<double> PFR1dSolver::solutionVector()
+{
+    // TODO make this with STL algorithm.
+    const double* sol = m_solver->solutionVector();
+    for (unsigned i = 0; i != m_vec.size(); ++i) { m_vec[i] = sol[i]; }
+    return m_vec;
+}
+
+vector<string> PFR1dSolver::variablesNames() const
+{
+    return m_var;
+}
+
+void PFR1dSolver::writeResults(const string & saveas)
+{
+    std::ofstream ofs(saveas, std::ios::out | std::ios::binary);
+    if (!ofs)
+    {
+        throw std::runtime_error("Cannot output to file");
+    }
+    ofs << m_ss.str();
+    ofs.close();
+}
+
+} 
+
--- src/pfr1d_solver.h
+++ src/pfr1d_solver.h
@@ -3,129 +3,39 @@
 ***************************************************************************
 */
 
-#ifndef HTRCT_SOLVER_1D
-#define HTRCT_SOLVER_1D
+#ifndef HTRCT_SOLVER_1D_H
+#define HTRCT_SOLVER_1D_H
 
 #include "pfr1d.h"
 #include "cantera/numerics/IDA_Solver.h"
-
-using namespace Cantera;
-using namespace std;
 
 namespace HeteroCt {
 
 class PFR1dSolver
 {
 public:
-    PFR1dSolver(PFR1d* pfr)
-    {
-        m_neq = pfr->nEquations();
-        m_vec.resize(m_neq);
-        m_var = pfr->variablesNames();
+    PFR1dSolver(PFR1d* pfr);
 
-        try
-        {
-            m_solver = new IDA_Solver {*pfr};
-            m_solver->setJacobianType(0);
-            m_solver->setDenseLinearSolver();
-            m_solver->init(0.0);
-        }
-        catch (Cantera::CanteraError& err)
-        {
-            std::cerr << err.what() << std::endl;
-        }
-    }
+    ~PFR1dSolver();
 
-    ~PFR1dSolver()
-    {
-        if (m_solver != nullptr) delete m_solver;
-    }
+    void setTolerances(double rtol, double atol);
 
-    void setTolerances(double rtol, double atol)
-    {
-        m_solver->setTolerances(rtol, atol);
-    }
+    void setMaxNumSteps(unsigned maxsteps);
 
-    void setMaxNumSteps(unsigned maxsteps)
-    {
-        m_solver->setMaxNumSteps(maxsteps);
-    }
+    void setInitialStepSize(double h0);
 
-    void setInitialStepSize(double h0)
-    {
-        m_solver->setInitialStepSize(h0);
-    }
+    void setStopPosition(double tstop);
 
-    void setStopPosition(double tstop)
-    {
-        m_solver->setStopTime(tstop);
-    }
+    int solve(double xout);
 
-    int solve(double xout)
-    { 
-        int retcode = 0;
+    double solution(unsigned num) const;
 
-        if (!m_ss_started)
-        {
-            for (auto var : m_var) { m_ss << var << ","; }
-            m_ss << "x" << std::endl;
+    std::vector<double> solutionVector();
 
-            for (unsigned i = 0; i != m_neq; ++i) {
-                m_ss << m_solver->solution(i) << ",";
-            }
-            m_ss << 0.0 << std::endl;
+    std::vector<std::string> variablesNames() const;
 
-            m_ss_started = true;
-        }
+    void writeResults(const std::string & saveas);
 
-        // TODO Manage return codes from IDA_Solver.solve.
-        try
-        {
-            retcode = m_solver->solve(xout);
-        }
-        catch (CanteraError& err)
-        {
-            std::cerr << err.what() << std::endl;
-            retcode = -99;
-        }
-
-        // TODO get pointer to sol instead.
-        for (unsigned i = 0; i != m_neq; ++i) {
-            m_ss << m_solver->solution(i) << ",";
-        }
-        m_ss << xout << std::endl;
-
-        return retcode;
-    }
-
-    double solution(unsigned num) const
-    {
-        return m_solver->solution(num);
-    }
-
-    std::vector<double> solutionVector()
-    {
-        // TODO make this with STL algorithm.
-        const double* sol = m_solver->solutionVector();
-        for (unsigned i = 0; i != m_vec.size(); ++i) { m_vec[i] = sol[i]; }
-        return m_vec;
-    }
-
-    std::vector<std::string> variablesNames() const
-    {
-        return m_var;
-    }
-
-    void writeResults(const std::string & saveas)
-    {
-        std::ofstream ofs(saveas, std::ios::out | std::ios::binary);
-        if (!ofs)
-        {
-            throw std::runtime_error("Cannot output to file");
-        }
-        ofs << m_ss.str();
-        ofs.close();
-    }
 protected:
     //! To provide access to results.
     std::vector<double> m_vec;
@@ -134,7 +44,7 @@ protected:
     std::vector<std::string> m_var;
 
     //! Pointer to IDA solver.
-    IDA_Solver* m_solver;
+    Cantera::IDA_Solver* m_solver;
 
     //! Buffer for results file.
     std::stringstream m_ss;
