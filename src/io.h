4891b8edb1cb96fe051fc5bcb8fa4b874c48736d
Bharat Medasani <mbkumar@gmail.com>
2019-05-01 18:05:45 -0400

    Doxygen comments added to io.h

--- src/io.h
+++ src/io.h
@@ -1,3 +1,9 @@
+/**
+ * @file io.h
+ */
+
+// This file is part of Hetero_ct.
+
 #ifndef HTRCT_IO_H
 #define HTRCT_IO_H
 
@@ -13,35 +19,65 @@
 namespace HeteroCt
 {
 
+/**
+ * Utility function to print formation enthalpies of species
+ */
 void print_formation_enthalpy(std::vector<Cantera::ThermoPhase*> phases, 
                               std::string output_file);
 
+/**
+ * Utility function to print formation entropies of species
+ */
 void print_formation_entropy(std::vector<Cantera::ThermoPhase*> phases, 
                              std::string output_file);
 
+/**
+ * Utility function to print reaction enthalpies
+ */
 void print_rxn_enthalpy(std::vector<Cantera::Kinetics*> kinetic_mgrs, 
                         doublereal T, 
                         std::string output_file);
 
+/**
+ * Utility function to print reaction entropies
+ */
 void print_rxn_entropy(std::vector<Cantera::Kinetics*> kinetic_mgrs, 
                        std::string output_file);
 
+/**
+ * Utility function to print equilibrium constants of reactions 
+ */
 void print_rxn_eq_consts(std::vector<Cantera::Kinetics*> kinetic_mgrs, 
                          std::string output_file);
 
+/**
+ * Utility function to print Gibbs energy of reactions 
+ */
 void print_rxn_gibbs(std::vector<Cantera::Kinetics*> kinetic_mgrs, 
                      doublereal T,  
                      std::string output_file);
 
+/**
+ * Utility function to print equilibrium constants of reactions 
+ */
 void print_rxn_kc(std::vector<Cantera::Kinetics*> kinetic_mgrs, 
                   std::string output_file);
 
+/**
+ * Utility function to print forward rate constants of reactions 
+ */
 void print_rxn_kf(std::vector<Cantera::Kinetics*> kinetic_mgrs, 
                   std::string output_file);
 
+/**
+ * Utility function to print reverse rate constants of reactions 
+ */
 void print_rxn_kr(std::vector<Cantera::Kinetics*> kinetic_mgrs, 
                   std::string output_file);
 
+/**
+ * Utility function to print hetero_ct header 
+ */
 void print_htrct_header(std::ostream& out); 
 
 
