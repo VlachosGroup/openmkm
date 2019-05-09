2db583cfdb512c14a81bea430be7807bbe3b9eeb
Bharat Medasani <mbkumar@gmail.com>
2019-05-09 10:24:32 -0400

    User given step size option enabled

--- src/util.cpp
+++ src/util.cpp
@@ -7,10 +7,10 @@ using namespace std;
 namespace HeteroCt
 {
 
-std::vector<double> get_log_intervals(double end_val)
+std::vector<double> get_log10_intervals(double end_val, double initial_step)
 {
     std::vector<double> intervals;
-    auto scale = 1e-12;
+    auto scale = initial_step;
     double r_val = 0;
     while (r_val < end_val) {
         for (size_t i=1; i < 10; i++){
@@ -19,8 +19,10 @@ std::vector<double> get_log_intervals(double end_val)
                 break;
             intervals.push_back(r_val);
         }
-        if (r_val > end_val)
+        if (r_val > end_val){
+            intervals.push_back(end_val);
             break;
+        }
         scale *= 10;
     }
     return intervals;
@@ -33,7 +35,7 @@ std::vector<double> get_reg_intervals(double start_val, double end_val, double s
     while (r_val < end_val) {
         r_val += step;
         if (r_val > end_val){
-            intervals.push_back(r_val);
+            intervals.push_back(end_val);
             break;
         }
         intervals.push_back(r_val);
--- src/util.h
+++ src/util.h
@@ -6,7 +6,7 @@
 namespace HeteroCt
 {
 
-std::vector<double> get_log_intervals(double end_val);
+std::vector<double> get_log10_intervals(double end_val, double initial_step);
 
 std::vector<double> get_reg_intervals(double start_val, double end_val, double step);
 
