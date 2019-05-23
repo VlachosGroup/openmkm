0de578af6216ffdea64d9ba6f5aa781a2b46ef56
Bharat Medasani <mbkumar@gmail.com>
2019-05-23 16:40:55 -0400

    First working commit of KINSol

--- src/NonLinearSolver.cpp
+++ src/NonLinearSolver.cpp
@@ -1,26 +1,24 @@
-//! @file NonLinearsolver.cpp Factory routine for picking the NonLinear solver package
+//! @file ODE_integrators.cpp
 
-// This file is part of OpenMKM. See License.txt in the top-level directory.
+// This file is part of Cantera. See License.txt in the top-level directory or
+// at http://www.cantera.org/license.txt for license and copyright information.
 
 #include "cantera/base/ct_defs.h"
-#include "cantera/numerics/NonLinearSolver.h"
-#include "cantera/numerics/KIN_Solver.h"
-
-// DAE_DEVEL is turned off at the current time
-#define DAE_DEVEL
-#ifdef DAE_DEVEL
+#include "NonLinearSolver.h"
+#include "KIN_Solver.h"
 
 namespace Cantera
 {
-NonLinearSolver* newNonLinearSolver(const std::string& itype, ResidJacEval& f)
+
+NonLinearSolver* newNonLinearSolver(const std::string& itype)
 {
-    if (itype == "KIN") {
-        return new KIN_Solver(f);
+    if (itype == "KINSOL") {
+        return new KIN_Solver();
     } else {
         throw CanteraError("newNonLinearSolver",
-                           "unknown Nonlinear solver: "+itype);
+                           "unknown nonlinear solver: "+itype);
     }
-}
+    return 0;
 }
 
-#endif
+}
