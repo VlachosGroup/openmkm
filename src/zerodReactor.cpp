fe02d7e435c3dba9a63fb58d158219c8f11b4dd1
Bharat Medasani <mbkumar@gmail.com>
2019-05-24 11:58:27 -0400

    Hybrid reactornet works in place of cantera reactornet for standard cases

--- src/zerodReactor.cpp
+++ src/zerodReactor.cpp
@@ -29,6 +29,7 @@
 #include "run_reactor.h"
 #include "omkmexceptions.h"
 #include "io.h"
+#include "ReactorNetHybrid.h"
 
 
 namespace OpenMKM 
@@ -197,7 +198,8 @@ void run_0d_reactor(ReactorParser& rctr_parser,
     cout << "Size of time vector " << times.size() << endl;
     
     // Setup simulation 
-    ReactorNet rnet; 
+    //ReactorNet rnet; 
+    ReactorNetHybrid rnet; 
     rnet.addReactor(*rctr);
 
     // Pass any user defined numerical options to the solver
