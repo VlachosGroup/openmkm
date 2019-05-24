998a62848fc4ed6d6a4f86b52c15af0e105a5a42
Bharat Medasani <mbkumar@gmail.com>
2019-05-24 10:42:36 -0400

    Redundant code for interface and interface_interactions removed

--- src/run_reactor.h
+++ src/run_reactor.h
@@ -27,12 +27,6 @@ void run_0d_reactor(ReactorParser& rctr_parser,
                     std::vector<std::shared_ptr<Cantera::InterfaceInteractions>> surfaces,
                     std::ofstream& gen_info);
 
-void run_0d_reactor( ReactorParser& rctr_parser,
-                    std::shared_ptr<Cantera::IdealGasMix> gas, 
-                    std::vector<std::shared_ptr<Cantera::Interface>> surfaces,
-                    std::ofstream& gen_info);
-
-
 void run_1d_reactor(ReactorParser& rctr_parser,
                     std::shared_ptr<Cantera::IdealGasMix> gas, 
                     std::vector<std::shared_ptr<Cantera::Interface>> surfaces,
